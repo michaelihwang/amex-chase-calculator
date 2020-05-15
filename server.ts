c31673d49338617ba3ca8e31767d4ed0369efa41
Michael Hwang <michaelinjeyhwang@gmail.com>
2020-05-15 14:03:37 -0700

    wrong favicon path causing server crashh

--- server.ts
+++ server.ts
@@ -6,7 +6,7 @@ const port = process.env.PORT || 8080;
 const app = express();
 
 // the __dirname is the current directory from where the script is running
-app.use(favicon(__dirname + '/src/assets/favicon.ico'));
+app.use(favicon(__dirname + '/public/favicon.ico'));
 
 app.use(express.static(__dirname, { dotfiles: 'allow' }));
 app.use(express.static(path.join(__dirname, 'build')));
